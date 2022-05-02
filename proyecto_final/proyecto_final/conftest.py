ad6c9493da1b3ca6894e4ed493a8fabb27a5ec3b
nicolas.agustin <nicolas.agustin.alvarado@gmail.com>
2022-05-02 19:45:51 -0300

    primer commit

---
+++ proyecto_final/proyecto_final/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from proyecto_final.users.models import User
+from proyecto_final.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
